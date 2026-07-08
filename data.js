f8fbc59c021e81962d66b35441b481d41769f1fd
Justin <justin@pingbit.co>
2026-07-09 04:39:23 +0800

    auto update Thu 09/07/2026  4:39:23.46

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.EC = {
-  STAMP: "8 Jul 2026 close · synced 9 Jul 04:09 KUL",
+  STAMP: "8 Jul 2026 close · synced 9 Jul 04:39 KUL",
   DATA: [
   {t:"PANW", c:"Palo Alto Networks", s:"Cybersecurity", d:"2026-08-24", conf:true, fq:"Q4 FY26", et:"", px:320.59, chg:-4.88, ytd:74.04, vol:53.6, sup:139.57, res:368.17, hi6:368.17, ath:false},
   {t:"AVGO", c:"Broadcom", s:"Semiconductor", d:"2026-09-03", conf:true, fq:"Q3 FY26", et:"5:00 PM", px:388.69, chg:4.83, ytd:12.72, vol:54.9, sup:289.5, res:494.22, hi6:494.22, ath:false},
