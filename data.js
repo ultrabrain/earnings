312b1e90cfde6aa0a16641f5b6a38a24a051a782
Justin <justin@pingbit.co>
2026-09-10 05:09:18 +0800

    auto update Thu 10/09/2026  5:09:16.48

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.EC = {
-  STAMP: "9 Sep 2026 close · synced 10 Sep 05:00 KUL",
+  STAMP: "9 Sep 2026 close · synced 10 Sep 05:09 KUL",
   DATA: [
 {t:"INTU", c:"Intuit", s:"Enterprise SaaS", d:"2026-11-19", conf:true, fq:"Q1 FY27", et:"4:30 PM", px:313.94, chg:-1.56, ytd:-52.16, vol:45.5, sup:251.72, res:471.22, hi6:471.22, ath:false},
 {t:"CRM", c:"Salesforce", s:"Enterprise SaaS", d:"2026-12-02", conf:false, fq:"Q2 FY27", et:"5:00 PM", px:244.16, chg:-1.99, ytd:-7.36, vol:58.4, sup:146.32, res:268.27, hi6:268.27, ath:false},
